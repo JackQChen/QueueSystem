8cb5774015b382782f9ae307b85ceaf4bce9cbc4
JackChen <121955443@qq.com>
2018-02-07 09:04:52 +0800

    fix

---
+++ SystemConfig_Web/SystemConfig/process/GridData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebFramework;
+using BLL;
+
+namespace SystemConfig.process
+{
+    public class GridData : ProcessBase
+    {
+        public string GetGridData(string bllName)
+        {
+            var tp = Type.GetType("BLL." + bllName + "BLL,BLL");
+            var obj = Activator.CreateInstance(tp);
+            var strResult = tp.GetMethod("GetGridData").Invoke(obj, null).ToJsonString();
+            return strResult;
+        }
+    }
+}
