31a048867e9903fbc1b51c9cb74098d2548479f2
JackChen <121955443@qq.com>
2018-02-24 17:40:17 +0800

    fix

--- SystemConfig_Web/SystemConfig/Controllers/HomeController.cs
+++ SystemConfig_Web/SystemConfig/Controllers/HomeController.cs
@@ -8,12 +8,25 @@ namespace SystemConfig.Controllers
 {
     public class HomeController : Controller
     {
+        static Dictionary<string, string> dicMenu;
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            return View();
+            if (dicMenu == null)
+            {
+                dicMenu = new Dictionary<string, string>();
+                dicMenu.Add("TUnit", "单位管理");
+                dicMenu.Add("TBusiness", "业务维护");
+                dicMenu.Add("TWindowArea", "区域维护");
+                dicMenu.Add("TUser", "用户维护");
+                dicMenu.Add("TWindow", "窗口维护");
+                dicMenu.Add("TBusinessAttribute", "业务属性扩展");
+                dicMenu.Add("TLedController", "LED控制卡维护");
+                dicMenu.Add("TLedWindow", "LED屏幕维护");
+            }
+            return View(dicMenu);
         }
 
     }
